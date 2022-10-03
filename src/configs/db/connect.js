e567e6d475b8ea7ebed48d0deac1bda66fc15e7c
Marcelo Cruz <mfugissecruz@Marcelos-Mac-mini.local>
2022-10-03 10:13:05 -0300

    teste conexao bd

--- src/configs/db/connect.js
+++ src/configs/db/connect.js
@@ -1,10 +1,21 @@
-const pg = require("pg-promise")();
-const database = pg({
-  host: process.env.PG_HOST, //"localhost",
-  database: process.env.PG_DATABASE, //"cazastore",
-  user: process.env.PG_USER, //"postgres",
-  port: process.env.PG_PORT, //5432
-  password: process.env.PG_PASSWORD, //"12345678",
+// const pg = require("pg-promise")();
+// const database = pg({
+
+//   host: process.env.PG_HOST, //"localhost",
+//   database: process.env.PG_DATABASE, //"cazastore",
+//   user: process.env.PG_USER, //"postgres",
+//   port: process.env.PG_PORT, //5432
+//   password: process.env.PG_PASSWORD, //"12345678",
+// });
+
+// module.exports = database;
+
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = database;
+client.connect();
